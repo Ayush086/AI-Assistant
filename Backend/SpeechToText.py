9ef2e89522702a55fd9e398a6d2639b454e5aeba
Ayush Uttarwar <111568550+Ayush086@users.noreply.github.com>
2025-01-14 00:57:55 +0530

    Update SpeechToText.py
    removed errors + comments added

--- Backend/SpeechToText.py
+++ Backend/SpeechToText.py
@@ -18,7 +18,6 @@ env_vars = dotenv_values(os.path.join(os.getcwd(), ".env"))
 # get input language from environment variables
 InputLanguage = env_vars.get("InputLanguage")
 
-
 #Define the HTML code for the speech recognition interface. 
 HtmlCode = '''<!DOCTYPE html>
 <html lang="en">
@@ -72,10 +71,12 @@ Link = f"{current_dir}/Data/Voice.html"
 #Set Chrome options for the WebDriver.
 chrome_options = Options()
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.142.86 Safari/537.36" 
-chrome_options.add_argument(f'user-agent-{user_agent}') 
+chrome_options.add_argument(f'user-agent={user_agent}') 
 chrome_options.add_argument("--use-fake-ui-for-media-stream")
 chrome_options.add_argument("--use-fake-device-for-media-stream")
-chrome_options.add_argument("--headless-new")
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--headless")
 #Initialize the Chrome WebDriver using the ChromeDriverManager. 
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=chrome_options)
@@ -91,16 +92,21 @@ def SetAssistantStatus(Status):
 # function to modify query to ensure proper punctuations
 def QueryModifier(query):
     new_query = query.lower().strip()
+    query_words = new_query.split()
     question_words = ['how', 'what', 'who', 'where', 'when', 'why', 'which', 'whose', 'whom', 'can you', "what's", "where's", "how's", "who's"]
     
     # Check if it's a question
-    if any(word in new_query for word in question_words):
-        if not new_query.endswith("?"):
-            new_query = new_query.rstrip(".?!") + "?"
+    if any(word + " " in new_query for word in question_words):
+        if query_words[-1][-1] in ['.', '?', '!']:
+            new_query = new_query[:-1] + "?"
+        else: 
+            new_query += "?"
     else:
-        # Ensure the query ends with a period if it's not a question
-        if not new_query.endswith("."):
-            new_query = new_query.rstrip(".?!") + "."
+        # add period if query is not a quesiton
+        if query_words[-1][-1] in ['.', '?', '!']:
+            new_query = new_query[:-1] + "."
+        else:
+            new_query += ". "
            
     return new_query.capitalize()
 
@@ -108,7 +114,7 @@ def QueryModifier(query):
 #Function to translate text into English using the translate library. 
 def UniversalTranslator(Text):
     english_translation = mt.translate(Text, "en", "auto")
-    return english_translation[:1].upper() + english_translation[1:]
+    return english_translation.capitalize()
 
 
 #Function to perform speech recognition using the WebDriver. 
